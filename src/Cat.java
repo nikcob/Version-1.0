3701e7fb414f8ab914189669ee14c38f67e7d798
nikcob <nikcob1@gmail.com>
2019-12-03 13:03:49 +0200

    Cat

---
+++ src/Cat.java
@@ -0,0 +1,9 @@
+public class Cat {
+    String name;
+
+
+    public void meow(String food) {
+        System.out.print("Meow, Menya zovut " + name);
+        System.out.println(food);
+    }
+}
---
+++ src/ProjectCats.java
@@ -0,0 +1,8 @@
+public class ProjectCats {
+    public static void main(String[] args) {
+
+        Cat myCat = new Cat();
+        myCat.name = "Tomas.";
+        myCat.meow(" I em grechku");
+    }
+}
